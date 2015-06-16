0838412c9e6cf27c8e7fe0692979b787e2b59d9d
Kaya <zengkaiyao@gmail.com>
2015-06-17 06:33:42 +0800

    test

---
+++ src/Token.java
@@ -0,0 +1,9 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: kaya
+ * Date: 6/17/15
+ * Time: 6:33 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class Token {
+}
