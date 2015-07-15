db5587b79b84106114e068847fde88937ef4a5d8
Kaya <zengkaiyao@gmail.com>
2015-07-16 07:04:32 +0800

    关联Java语言

---
+++ src/runner/NativeRunner.java
@@ -0,0 +1,20 @@
+package runner;
+
+import javassist.gluonj.util.Loader;
+import stitch.ClosureEvaluator;
+import stitch.NativeEvaluator;
+import stitch.NativeInterpreter;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: kaya
+ * Date: 7/16/15
+ * Time: 6:55 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class NativeRunner {
+    public static void main(String[] args) throws Throwable{
+        Loader.run(NativeInterpreter.class,args, NativeEvaluator.class, ClosureEvaluator.class);
+    }
+
+}
